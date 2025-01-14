dbe5a4a34f1642a33e075eb03f1cb8af36c11ce5
Erik-Kipu <ervinmalyshev@gmail.com>
2025-01-14 20:25:04 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница Футбола */
+router.get('/football', function(req, res, next) {
+  res.send("<h1>Страница Футбола</h1>")
+  });
+  /* Страница Баскетбола */
+router.get('/basketball', function(req, res, next) {
+  res.send("<h1>Страница Баскетбола</h1>")
+  });
+  /* Страница Тенниса */
+  router.get('/tennis', function(req, res, next) {
+  res.send("<h1>Страница Тенниса</h1>")
+  });
 
 module.exports = router;
