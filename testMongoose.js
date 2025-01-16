e7f792f5ec27f671d9f254f2d79b8b7191fe2b1b
Erik-Kipu <ervinmalyshev@gmail.com>
2025-01-16 13:24:03 +0300

    tc_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Sport = mongoose.model('Sport', { name: String });
+var schema = mongoose.Schema({ name: String })
+schema.methods.meow = function(){
+console.log(this.name + " сказал гол")
+}
+const Sport = mongoose.model('Sport', schema);
 const sporting = new Sport({ name: 'Хоккей' });
-sporting.save().then(() => console.log('Гол'));
+sporting.save().then(() => sporting.meow());
